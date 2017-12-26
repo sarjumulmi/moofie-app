2de28f25b6698a18daeb388646355dc0628b6485
sarjumulmi <mulmi.sarju@gmail.com>
2017-12-25 22:27:03 -0500

    List and Movie models association changed to has_and_belongs_to_many, join table deleted.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171224145721) do
+ActiveRecord::Schema.define(version: 20171224143706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,6 @@ ActiveRecord::Schema.define(version: 20171224145721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
-  end
-
-  create_table "lists_movies", id: false, force: :cascade do |t|
-    t.bigint "movie_id", null: false
-    t.bigint "list_id", null: false
-    t.index ["movie_id", "list_id"], name: "index_lists_movies_on_movie_id_and_list_id"
   end
 
   create_table "movies", force: :cascade do |t|
