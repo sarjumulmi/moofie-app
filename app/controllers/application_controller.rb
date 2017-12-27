562cf155f77b1370e380d6120cc18b6c70ab73cf
sarjumulmi <mulmi.sarju@gmail.com>
2017-12-27 10:28:00 -0500

    unauthorized access tested. Custom unauthorized error message created.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   include Knock::Authenticable
 
   protect_from_forgery with: :exception
-  
+
+  private
+
+  def unauthorized_entity(entity_name)
+    render json: {errors: {messages: ["#{entity_name.titleize} is Unauthorized"] }}, status: :unauthorized
+  end
+
 end
--- spec/requests/list_request_spec.rb
+++ spec/requests/list_request_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Lists API', type: :request do
 
   describe 'GET /api/lists' do
     context 'Authorized user' do
-      let!(:user_attributes1) {attributes_for(:user)}
+      let(:user_attributes1) {attributes_for(:user)}
       let!(:user1) {User.create(user_attributes1)}
-      let!(:user_attributes2) {attributes_for(:user)}
+      let(:user_attributes2) {attributes_for(:user)}
       let!(:user2) {User.create(user_attributes2)}
       let!(:lists1) {create_list(:list, 4, user:user1)}
       let!(:lists2) {create_list(:list, 2, user:user2)}
@@ -21,6 +21,7 @@ RSpec.describe 'Lists API', type: :request do
         post '/api/user_token', params: {"auth": {"email": user_attributes1[:email], "password": "password"}}
         get '/api/lists', headers: auth_header
       end
+
       it "responds with a status of 201" do
         expect(response).to have_http_status(201)
       end
@@ -30,8 +31,27 @@ RSpec.describe 'Lists API', type: :request do
         expect(lists[2][:user_id]).to eq(user1.id)
         expect(lists.map { |l| l[:title] }).not_to include(lists2.map { |l| l[:title]  })
       end
-
     end
+
+    context 'Unauthorized user' do
+      let(:user_attributes) {attributes_for(:user)}
+      let!(:user) {User.create(user_attributes)}
+      let!(:lists) {create_list(:list, 4, user:user)}
+      before {get '/api/lists'}
+
+      it "returns a status of 401" do
+        expect(response).to have_http_status(401)
+      end
+      it "returns an error message" do
+        json = JSON.parse(response.body, symbolize_names: true)
+        expect(json[:errors][:messages]).to eq(["User is Unauthorized"])
+      end
+    end
+
+  end
+
+  describe 'POST /api/lists' do
+
   end
 
 end
