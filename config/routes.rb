1d766cbb5bc94ccee2fee13abe88b6788fbf179a
sarjumulmi <mulmi.sarju@gmail.com>
2018-01-01 11:50:33 -0500

    webpacker and webpacker:react installed. Basic react app configured.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
   namespace :api do
     post '/signup', to: 'users#create'
     post '/user_token' => 'user_token#create'
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
       resources :movies, only: [:index, :create, :show]
     end
   end
+
+  get "*path", to: "welcome#index"
+
 end
