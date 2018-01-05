8e98c248aa89b1ef92c382116342ee15bb61773c
sarjumulmi <mulmi.sarju@gmail.com>
2018-01-05 13:18:50 -0500

    navbar styled with semantic Menu component. Switch added to App routes.

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './views/home/'
 import Navbar from './views/navbar/'
 import SignupForm from './views/signup'
@@ -9,10 +9,12 @@ import LogoutForm from './views/logout'
 const App = () => (
   <div>
     <Navbar />
-    <Route exact path='/' component={Home} />
-    <Route path='/signup' component={SignupForm} />
-    <Route path='/login' component={LoginForm} />
-    <Route path='/logout' component={LogoutForm} />
+    <Switch>
+      <Route path='/signup' component={SignupForm} />
+      <Route path='/login' component={LoginForm} />
+      <Route path='/logout' component={LogoutForm} />
+      <Route path='/' component={Home} />
+    </Switch>
   </div>
 )
 
--- app/javascript/packs/views/navbar/index.jsx
+++ app/javascript/packs/views/navbar/index.jsx
@@ -1,29 +1,42 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Menu, Container } from 'semantic-ui-react'
 
 class Navbar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeItem: ''
+    }
+  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render () {
+    const {activeItem} = this.state
     const userLinks = (
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/logout'>Logout</Link>
-      </div>
+      <Menu size='large' inverted fixed='top' style={{height: '65px'}}>
+        <Menu.Item as={Link} to='/' active={activeItem==='home'} name='home' onClick={this.handleItemClick}>Home</Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item as={Link} to='/logout' active={activeItem==='logout'} name='logout' position='right' onClick={this.handleItemClick}>Logout</Menu.Item>
+        </Menu.Menu>
+      </Menu>
     )
     const guestLinks = (
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/signup'>Sign Up</Link>
-        <Link to='/login'>Login</Link>
-      </div>
+      <Menu size='large' inverted fixed='top' style={{height: '65px'}}>
+        <Menu.Item as={Link} to='/' active={activeItem==='home'} name='home' onClick={this.handleItemClick}>Home</Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item as={Link} to='/signup' active={activeItem==='signup'} name='signup' position='right' onClick={this.handleItemClick}>Sign Up</Menu.Item>
+          <Menu.Item as={Link} to='/login' active={activeItem==='login'} name='login' position='right' onClick={this.handleItemClick}>Login</Menu.Item>
+        </Menu.Menu>
+      </Menu>
     )
     const {isAuthenticated} = this.props.auth
     return (
-      <div>
-        <nav>
+      <Container>
+        <div style={{margin: '0 0 100px'}}>
           {isAuthenticated ? userLinks : guestLinks}
-        </nav>
-      </div>
+        </div>
+      </Container>
     )
   }
 }
