0632fe28f1481c38f1710b7e7f0922fe956be554
sarjumulmi <mulmi.sarju@gmail.com>
2018-01-06 21:20:09 -0500

    Search results displayed as MovieLinkLists. Each MovieLink is routed to '/movies/:movieId'.

--- app/javascript/packs/views/home/index.jsx
+++ app/javascript/packs/views/home/index.jsx
@@ -1,8 +1,12 @@
 import React, {Component} from 'react'
 import SearchBar from './../search/'
-import { Header } from 'semantic-ui-react'
+import MovieLinkList from './../movieLinkList/'
+import { Message, Header, Grid } from 'semantic-ui-react'
 import {connect} from 'react-redux';
 import { getMovies } from './../../actions/searchActions';
+import isEmpty from 'lodash/isEmpty'
+import axios from 'axios'
+import { Route } from 'react-router-dom';
 
 class Home extends Component {
   constructor (props) {
@@ -10,36 +14,60 @@ class Home extends Component {
     this.state = {
       queryTerm: '',
       isLoading: false,
-      movies:{}
+      movies:{},
+      errors: ''
     }
   }
   onChange = (e) => {
     this.setState({
       queryTerm: e.target.value,
-      isLoading:true,
-      movies:{}
+      isLoading:true
     })
   }
   onSubmit = (e) => {
     e.preventDefault()
+    this.props.history.push('/movies')
+    this.setState({errors:'', movies:{}})
     this.props.getMovies(this.state.queryTerm).then(
       data => {
-        this.setState({isLoading: false, movies: data})
+        if (isEmpty(data)) {
+          this.setState({isLoading: false, errors: 'No results found. Please try again!!'})
+        } else {
+          this.setState({isLoading: false, errors: '', movies: data})
+        }
       }
     ).catch((err) => {
-      console.log(err)
-      this.setState({
-        isLoading: false
-      })
+      this.setState({isLoading: false, errors: 'No results found. Please try again!!'})
     })
   }
 
   render () {
-    console.log(this.state)
+    const matchPath = this.props.match.path
     return (
       <div style={{margin: '0 0 0 10px'}}>
         <Header as='h3'>Recent Movies having API {process.env.MOVIE_DB_API_KEY} {process.env.NODE_ENV} environment.</Header>
-        <SearchBar queryTerm={this.state.queryTerm} onSubmit={this.onSubmit} onChange={this.onChange} loading={!!this.state.queryTerm && this.state.isLoading}/>
+        <SearchBar
+          queryTerm={this.state.queryTerm}
+          onSubmit={this.onSubmit}
+          onChange={this.onChange}
+          loading={!!this.state.queryTerm && this.state.isLoading}
+          />
+        {!!this.state.errors ? <Message compact warning={!!this.state.errors} header='Error' content={this.state.errors} /> : null}
+        <Grid>
+          <Grid.Column width={6}>
+            <MovieLinkList movies={this.state.movies} moviePath={matchPath} />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Route path={`${matchPath}/:movieId`} render={({match}) => {
+                const movieId = match.params.movieId
+                const movie = this.state.movies.movieId
+                return (
+                  <div>{this.state.movies[movieId].title}</div>
+                )
+              }}
+            />
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
