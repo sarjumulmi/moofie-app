cf51a48a7d65b549369a1af04fb6c9f0bd20940f
sarjumulmi <mulmi.sarju@gmail.com>
2018-01-12 17:34:25 -0500

    list reducer deleted.

--- app/javascript/packs/rootReducer.js
+++ app/javascript/packs/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import moviesById from './reducers/moviesById'
-import listsById from './reducers/listsById'
 import auth from './reducers/auth'
 
 export default combineReducers({
   moviesById,
-  listsById,
   auth
 })
