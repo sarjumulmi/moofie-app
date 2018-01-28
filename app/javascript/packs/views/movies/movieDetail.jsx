56ec9d2b687578767cb21a33fed76f2842c88797
sarjumulmi <mulmi.sarju@gmail.com>
2018-01-28 15:23:52 -0500

    Number of movies displayed with My List menu item. MovieDetail edited to contain 'no movie found' fall back message.

--- app/javascript/packs/views/movies/movieDetail.jsx
+++ app/javascript/packs/views/movies/movieDetail.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
-import {Segment, Image, Rating} from 'semantic-ui-react'
+import {Segment, Image, Rating, Header} from 'semantic-ui-react'
 import youtubeLogo from './../../public/images/youtube-logo.png'
 import netflixLogo from './../../public/images/netflix-logo.png'
 import googlePlayLogo from './../../public/images/google-play-logo.png'
 import PropTypes from 'prop-types'
 
-const MovieDetail = ({movie}) => (
-  <Segment style={{width: '80%', backgroundColor: '#03a9f414'}}>
-    <h2 style={{marginTop: '0', marginBottom: '0'}}>{movie.title}</h2>
-    <p><small> - {movie.tagline}</small></p>
-    <h4 style={{marginTop: '0', marginBottom: '0'}}>Genres</h4>
-    <p>{movie.genres} </p>
-    <h3 style={{marginTop: '0', marginBottom: '0'}}>Overview: </h3>
-    <p>{movie.overview}</p>
-    <h4 style={{marginTop: '0', marginBottom: '0'}}>Rating</h4>
-    <Rating icon='star' rating={Math.round(movie.rating/2)} maxRating={5} disabled />
-    <h4 style={{marginTop: '10px', marginBottom: '0'}}>Production Companies: </h4>
-    <p>{movie.production_companies}</p>
-    <h4 style={{marginTop: '0', marginBottom: '7px'}}>Available On</h4>
-    <p style={{marginBottom: '7px'}}><Image src={youtubeLogo} floated='left' />Youtube</p>
-    <p style={{marginBottom: '7px'}}><Image src={netflixLogo} floated='left' />Netflix</p>
-    <p style={{marginBottom: '7px'}}><Image src={googlePlayLogo} floated='left' />Google Play</p>
-  </Segment>
-)
+const MovieDetail = ({movie}) => {
+  if (movie) {
+    return (
+      <Segment style={{width: '80%', backgroundColor: '#03a9f414'}}>
+        <h2 style={{marginTop: '0', marginBottom: '0'}}>{movie.title}</h2>
+        <p><small> - {movie.tagline}</small></p>
+        <h4 style={{marginTop: '0', marginBottom: '0'}}>Genres</h4>
+        <p>{movie.genres} </p>
+        <h3 style={{marginTop: '0', marginBottom: '0'}}>Overview: </h3>
+        <p>{movie.overview}</p>
+        <h4 style={{marginTop: '0', marginBottom: '0'}}>Rating</h4>
+        <Rating icon='star' rating={Math.round(movie.rating / 2)} maxRating={5} disabled />
+        <h4 style={{marginTop: '10px', marginBottom: '0'}}>Production Companies: </h4>
+        <p>{movie.production_companies}</p>
+        <h4 style={{marginTop: '0', marginBottom: '7px'}}>Available On</h4>
+        <p style={{marginBottom: '7px'}}><Image src={youtubeLogo} floated='left' />Youtube</p>
+        <p style={{marginBottom: '7px'}}><Image src={netflixLogo} floated='left' />Netflix</p>
+        <p style={{marginBottom: '7px'}}><Image src={googlePlayLogo} floated='left' />Google Play</p>
+      </Segment>
+    )
+  } else {
+    return (
+      <Header as='h3'>No Matching Movie Found!!</Header>
+    )
+  }
+}
 
 MovieDetail.propTyes = {
   movie: PropTypes.shape({
--- app/javascript/packs/views/navbar/index.jsx
+++ app/javascript/packs/views/navbar/index.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Menu, Container, Image } from 'semantic-ui-react'
+import { Menu, Container, Image, Label } from 'semantic-ui-react'
 import logo from './../../public/images/logo.png'
 
 class Navbar extends Component {
@@ -13,8 +13,10 @@ class Navbar extends Component {
     }
   }
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render () {
     const {activeItem} = this.state
+    const movieNumber = Object.keys(this.props.moviesById).length
     const userLinks = (
       <Menu size='huge' inverted fixed='top' style={{height: '75px'}}>
         <Menu.Item style={{paddingRight: '2px', paddingLeft: '2px'}}>
@@ -24,7 +26,9 @@ class Navbar extends Component {
           Home
         </Menu.Item>
         <Menu.Menu position='right'>
-          <Menu.Item as={Link} to='/movies/list' active={activeItem==='list'} name='list' position='right' onClick={this.handleItemClick}>My List</Menu.Item>
+          <Menu.Item name='My List' as={Link} to='/movies/list' active={activeItem==='list'} name='list' position='right' onClick={this.handleItemClick}>
+            My List <span><Label color='yellow' floating style={{top: 'unset', bottom: '0.2em', left: '90%'}}>{movieNumber}</Label></span>  
+          </Menu.Item>
           <Menu.Item as={Link} to='/logout' active={activeItem==='logout'} name='logout' position='right' onClick={this.handleItemClick}>Logout</Menu.Item>
         </Menu.Menu>
       </Menu>
@@ -60,7 +64,8 @@ Navbar.propTypes = {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.auth
+    auth: state.auth,
+    moviesById: state.moviesById
   }
 }
 export default connect(mapStateToProps)(Navbar)
