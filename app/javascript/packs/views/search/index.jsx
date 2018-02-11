c442e17221648e3b489181687ea4e50cf82b67e5
sarjumulmi <mulmi.sarju@gmail.com>
2018-02-11 09:33:56 -0500

    Form and Input replaced with Search component. Autosuggest props passed.

--- app/javascript/packs/views/search/index.jsx
+++ app/javascript/packs/views/search/index.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
-import { Form, Grid, Icon, Image } from 'semantic-ui-react'
+import { Grid, Image, Search, Label } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import moofieTitle from './../../public/images/moofieTitle.png'
+
+const resultRenderer = ({title, release_date}) => {
+  const releaseYear = release_date.slice(0, 4) || 'Unreleased'
+  const content = `${title}, ${releaseYear}`
+  return <Label content={content} />
+}
 
 const SearchBar = (props) => (
   <div >
     <Grid>
       <Grid.Column width={13} style={{margin: 'auto'}}>
         <Image src={moofieTitle} style={{margin: '8px auto 12px auto'}} />
-        <Form onSubmit={props.onSubmit} loading={props.loading} style={{marginBottom: '12px'}}>
-          <Form.Input fluid
-            icon
-            placeholder='Search Movies...'
-            size='big'
-            value={props.queryTerm}
-            onChange={props.onChange} >
-            <input />
-            <Icon name='search' inverted circular link onClick={props.onSubmit} />
-          </Form.Input>
-        </Form>
+        <Search
+          loading={props.loading}
+          results={props.movieSearchResults}
+          value={props.queryTerm}
+          onSearchChange={props.onSearchChange}
+          onResultSelect={props.onResultSelect}
+          resultRenderer={resultRenderer}
+          minCharacters={2}
+          input={{icon: 'search', style: {width: '1000px', marginBottom: '10px'}}}
+          {...this.props}
+        />
       </Grid.Column>
     </Grid>
   </div>
 )
 
 SearchBar.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onResultSelect: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onSearchChange: PropTypes.func.isRequired,
   queryTerm: PropTypes.string.isRequired
 }
 
